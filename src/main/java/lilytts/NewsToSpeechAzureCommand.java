8a8898373af4a23f936b13e7661df49b580aacc9
Lily Haverstein <lilyhaverstein@outlook.com>
2022-08-22 16:17:12 -0700

    Working on sorting articles by save time.

--- src/main/java/lilytts/NewsToSpeechAzureCommand.java
+++ src/main/java/lilytts/NewsToSpeechAzureCommand.java
@@ -2,10 +2,14 @@ package lilytts;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.FileTime;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -13,7 +17,6 @@ import java.util.concurrent.Callable;
 
 import javax.xml.stream.XMLOutputFactory;
 
-import com.mpatric.mp3agic.ID3v1Tag;
 import com.mpatric.mp3agic.ID3v24Tag;
 import com.mpatric.mp3agic.Mp3File;
 
@@ -31,15 +34,32 @@ import picocli.CommandLine.Parameters;
 
 @Command(name = "news-to-speech-azure")
 public class NewsToSpeechAzureCommand implements Callable<Integer> {
+    private static class Article {
+        // TODO: Encapsulate these with getters.
+        private final String title;
+        private final String fileName;
+        private final String publisher;
+        private final FileTime savedDate;
+        private final List<ContentItem> content;
+
+        public Article(String title, String fileName, String publisher, FileTime savedDate, List<ContentItem> content) {
+            this.title = title;
+            this.fileName = fileName;
+            this.publisher = publisher;
+            this.savedDate = savedDate;
+            this.content = content;
+        }
+    }
+
     @Parameters(index = "0")
     private File inputDirectory;
-    
+
     @Parameters(index = "1")
     private File outputDirectory;
 
     @Option(names = { "--subscriptionKey" })
     private String subscriptionKey;
-    
+
     @Option(names = { "--serviceRegion" })
     private String serviceRegion;
 
@@ -64,57 +84,50 @@ public class NewsToSpeechAzureCommand implements Callable<Integer> {
 
         int currentTrackNumber = 1;
 
-        for (File publisherDirectory : inputDirectory.listFiles(x -> x.isDirectory())) {
-            final String publisherName = publisherDirectory.getName();
+        List<Article> articles = fetchArticles(contentParser, splitter);
 
-            for (File articleTextFile : publisherDirectory.listFiles(x -> x.getName().endsWith(".txt"))) {
-                final FileReader inputStream = new FileReader(articleTextFile);
-                final List<ContentItem> content = contentParser.readContent(inputStream);
-                final List<List<ContentItem>> parts = splitter.splitContent(content);
+        Collections.sort(articles, (x, y) -> x.savedDate.compareTo(y.savedDate));
 
-                final String articleName = content.stream()
-                    .filter(x -> x instanceof ChapterTitleContent)
-                    .map(x -> ((ChapterTitleContent) x).getContent())
-                    .findFirst()
-                    .orElseGet(() -> removeFileExtension(articleTextFile.getName()));
+        for (Article article : articles) {
+            final List<List<ContentItem>> parts = splitter.splitContent(article.content);
 
-                System.out.printf("Converting input file '%s' to speech as %s part(s).\n",
-                    articleTextFile.getName(),
+            System.out.printf("Converting article '%s' to speech as %s part(s).\n",
+                    article.title,
                     parts.size());
-                
-                // TODO: Share this code with TextToSpeechAzureCommand.
-                for (int i = 0; i < parts.size(); i++) {
-                    final String outputFileName = parts.size() > 1 ?
-                        removeFileExtension(articleTextFile.getName()) + " (Part " + (i+1) + ").mp3" :
-                        removeFileExtension(articleTextFile.getName()) + ".mp3";
-                    final File outputFile = new File(outputDirectory, outputFileName);
-                    final File tempOutputFile = new File(outputDirectory, removeFileExtension(outputFileName) + "_temp.mp3");
 
-                    tempOutputFile.deleteOnExit();
-    
-                    if (outputFile.exists() && outputFile.length() > 0) {
-                        System.out.printf("Skipping file '%s' because it already exists.\n", outputFile.getName());
-                        continue;
-                    }
-    
-                    final StringWriter ssmlStringWriter = new StringWriter();
-                    ssmlWriter.writeSSML(parts.get(i), xmlOutputFactory.createXMLStreamWriter(ssmlStringWriter));
-                    synthesizer.synthesizeSsmlToFile(tempOutputFile.getAbsolutePath(), ssmlStringWriter.toString());
+            // TODO: Share this code with TextToSpeechAzureCommand.
+            for (int i = 0; i < parts.size(); i++) {
+                final String outputFileName = parts.size() > 1
+                        ? article.fileName + " (Part " + (i + 1) + ").mp3"
+                        : article.fileName + ".mp3";
+                final File outputFile = new File(outputDirectory, outputFileName);
+                final File tempOutputFile = new File(outputDirectory,
+                        removeFileExtension(outputFileName) + "_temp.mp3");
 
-                    final ID3v24Tag metadata = new ID3v24Tag();
-                    metadata.setArtist(publisherName);
-                    metadata.setAlbumArtist("News Deep Dive");
-                    metadata.setAlbum(albumName);
-                    metadata.setTitle(articleName);
-                    metadata.setTrack(Integer.toString(currentTrackNumber));
+                tempOutputFile.deleteOnExit();
 
-                    Files.copy(tempOutputFile.toPath(), outputFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                    final Mp3File mp3File = new Mp3File(tempOutputFile.getAbsolutePath());
-                    mp3File.setId3v2Tag(metadata);
-                    mp3File.save(outputFile.getAbsolutePath());
-
-                    currentTrackNumber++;
+                if (outputFile.exists() && outputFile.length() > 0) {
+                    System.out.printf("Skipping file '%s' because it already exists.\n", outputFile.getName());
+                    continue;
                 }
+
+                final StringWriter ssmlStringWriter = new StringWriter();
+                ssmlWriter.writeSSML(parts.get(i), xmlOutputFactory.createXMLStreamWriter(ssmlStringWriter));
+                synthesizer.synthesizeSsmlToFile(tempOutputFile.getAbsolutePath(), ssmlStringWriter.toString());
+
+                final ID3v24Tag metadata = new ID3v24Tag();
+                metadata.setArtist(article.publisher);
+                metadata.setAlbumArtist("News Deep Dive");
+                metadata.setAlbum(albumName);
+                metadata.setTitle(article.title);
+                metadata.setTrack(Integer.toString(currentTrackNumber));
+
+                Files.copy(tempOutputFile.toPath(), outputFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                final Mp3File mp3File = new Mp3File(tempOutputFile.getAbsolutePath());
+                mp3File.setId3v2Tag(metadata);
+                mp3File.save(outputFile.getAbsolutePath());
+
+                currentTrackNumber++;
             }
         }
 
@@ -133,17 +146,17 @@ public class NewsToSpeechAzureCommand implements Callable<Integer> {
 
     private SSMLWriter configureSsmlWriter() {
         SSMLWriter.Builder builder = SSMLWriter.builder()
-            .withVoice(voice.getVoiceName());
+                .withVoice(voice.getVoiceName());
 
         if (!isNullOrEmpty(this.voice.getVoiceStyle())) {
             builder.withVoiceStyle(this.voice.getVoiceStyle());
         }
-        
+
         // TODO: Does this handle negative numbers?
         if (this.prosodyRate != 0) {
             builder = builder.withProsodyRate(String.format(Locale.ENGLISH, "%d%%", this.prosodyRate));
         }
-        
+
         return builder.build();
     }
 
@@ -160,5 +173,30 @@ public class NewsToSpeechAzureCommand implements Callable<Integer> {
         } else {
             return fileName;
         }
+    }
+
+    private List<Article> fetchArticles(ContentParser contentParser, ContentSplitter splitter) throws IOException {
+        final List<Article> articles = new ArrayList<>();
+
+        for (File publisherDirectory : inputDirectory.listFiles(x -> x.isDirectory())) {
+            final String publisherName = publisherDirectory.getName();
+
+            for (File articleTextFile : publisherDirectory.listFiles(x -> x.getName().endsWith(".txt"))) {
+                final FileReader inputStream = new FileReader(articleTextFile);
+                final List<ContentItem> content = contentParser.readContent(inputStream);
+                final FileTime savedDate = (FileTime) Files.getAttribute(articleTextFile.toPath(), "creationTime");
+                final String fileName = removeFileExtension(articleTextFile.getName());
+
+                final String articleName = content.stream()
+                        .filter(x -> x instanceof ChapterTitleContent)
+                        .map(x -> ((ChapterTitleContent) x).getContent())
+                        .findFirst()
+                        .orElseGet(() -> removeFileExtension(articleTextFile.getName()));
+
+                articles.add(new Article(articleName, fileName, publisherName, savedDate, content));
+            }
+        }
+
+        return articles;
     }
 }
