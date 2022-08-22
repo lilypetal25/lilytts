60e18b0ca22082b59c340ae286f01d90598248dd
Lily Haverstein <lilyhaverstein@outlook.com>
2022-08-22 09:42:57 -0700

    Tagging working.

--- src/main/java/lilytts/NewsToSpeechAzureCommand.java
+++ src/main/java/lilytts/NewsToSpeechAzureCommand.java
@@ -3,12 +3,20 @@ package lilytts;
 import java.io.File;
 import java.io.FileReader;
 import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.Callable;
 
 import javax.xml.stream.XMLOutputFactory;
 
+import com.mpatric.mp3agic.ID3v1Tag;
+import com.mpatric.mp3agic.Mp3File;
+
+import lilytts.content.ChapterTitleContent;
 import lilytts.content.ContentItem;
 import lilytts.parsing.ContentParser;
 import lilytts.parsing.text.TextContentParser;
@@ -50,11 +58,24 @@ public class NewsToSpeechAzureCommand implements Callable<Integer> {
         final ContentSplitter splitter = ContentSplitter.builder().withMaxPartCharacters(9000).build();
         final AzureSynthesizer synthesizer = AzureSynthesizer.fromSubscription(subscriptionKey, serviceRegion);
 
+        final String currentDateString = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        final String albumName = "News Deep Dive: " + currentDateString;
+
+        int currentTrackNumber = 1;
+
         for (File publisherDirectory : inputDirectory.listFiles(x -> x.isDirectory())) {
+            final String publisherName = publisherDirectory.getName();
+
             for (File articleTextFile : publisherDirectory.listFiles(x -> x.getName().endsWith(".txt"))) {
                 final FileReader inputStream = new FileReader(articleTextFile);
                 final List<ContentItem> content = contentParser.readContent(inputStream);
                 final List<List<ContentItem>> parts = splitter.splitContent(content);
+
+                final String articleName = content.stream()
+                    .filter(x -> x instanceof ChapterTitleContent)
+                    .map(x -> ((ChapterTitleContent) x).getContent())
+                    .findFirst()
+                    .orElseGet(() -> removeFileExtension(articleTextFile.getName()));
 
                 System.out.printf("Converting input file '%s' to speech as %s part(s).\n",
                     articleTextFile.getName(),
@@ -66,6 +87,9 @@ public class NewsToSpeechAzureCommand implements Callable<Integer> {
                         removeFileExtension(articleTextFile.getName()) + " (Part " + (i+1) + ").mp3" :
                         removeFileExtension(articleTextFile.getName()) + ".mp3";
                     final File outputFile = new File(outputDirectory, outputFileName);
+                    final File tempOutputFile = new File(outputDirectory, removeFileExtension(outputFileName) + "_temp.mp3");
+
+                    tempOutputFile.deleteOnExit();
     
                     if (outputFile.exists() && outputFile.length() > 0) {
                         System.out.printf("Skipping file '%s' because it already exists.\n", outputFile.getName());
@@ -74,7 +98,18 @@ public class NewsToSpeechAzureCommand implements Callable<Integer> {
     
                     final StringWriter ssmlStringWriter = new StringWriter();
                     ssmlWriter.writeSSML(parts.get(i), xmlOutputFactory.createXMLStreamWriter(ssmlStringWriter));
-                    synthesizer.synthesizeSsmlToFile(outputFile.getAbsolutePath(), ssmlStringWriter.toString());
+                    synthesizer.synthesizeSsmlToFile(tempOutputFile.getAbsolutePath(), ssmlStringWriter.toString());
+
+                    final ID3v1Tag metadata = new ID3v1Tag();
+                    metadata.setArtist(publisherName);
+                    metadata.setAlbum(albumName);
+                    metadata.setTitle(articleName);
+                    metadata.setTrack(Integer.toString(currentTrackNumber));
+
+                    Files.copy(tempOutputFile.toPath(), outputFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    final Mp3File mp3File = new Mp3File(tempOutputFile.getAbsolutePath());
+                    mp3File.setId3v1Tag(metadata);
+                    mp3File.save(outputFile.getAbsolutePath());
                 }
             }
         }
