52b2592ea63a4fa3ccd91bb8a590c6abf9ebfb1a
Lily Haverstein <lilyhaverstein@outlook.com>
2023-03-19 11:12:34 -0700

    Add support for Steffan voice.

--- src/main/java/lilytts/synthesis/AzureVoice.java
+++ src/main/java/lilytts/synthesis/AzureVoice.java
@@ -10,7 +10,8 @@ public enum AzureVoice {
     Libby("en-GB-LibbyNeural"),
     Sara("en-US-SaraNeural"),
     Jacob("en-US-JacobNeural"),
-    Jane("en-US-JaneNeural");
+    Jane("en-US-JaneNeural"),
+    Steffan("en-US-SteffanNeural");
 
     private final String voiceName;
 
