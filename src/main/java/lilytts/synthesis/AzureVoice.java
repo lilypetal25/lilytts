51d22600b702904acd80b67405c85f0af4389f8e
Lily Haverstein <lilyhaverstein@outlook.com>
2023-03-20 10:53:08 -0700

    Add support for Tony voice.

--- src/main/java/lilytts/synthesis/AzureVoice.java
+++ src/main/java/lilytts/synthesis/AzureVoice.java
@@ -11,7 +11,8 @@ public enum AzureVoice {
     Sara("en-US-SaraNeural"),
     Jacob("en-US-JacobNeural"),
     Jane("en-US-JaneNeural"),
-    Steffan("en-US-SteffanNeural");
+    Steffan("en-US-SteffanNeural"),
+    Tony("en-US-TonyNeural");
 
     private final String voiceName;
 
